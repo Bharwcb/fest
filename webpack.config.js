77bec477bd05be84caa86021a220de0a92964749
Brad Harris <bradharris@Brad-Harris-MacBook-Air.local>
2017-07-27 09:57:19 -0700

    renders App component

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
 	// in tutorial (no views folder):
 	// template: __dirname + '/app/index.html'
 	template: __dirname + '/app/views/index.html',
-	filename: 'index.html'
+	filename: 'index.html',
 	// When HTMLWebpackPlugin creates a new HTML file, that new HTML file will contain a <script> tag linking to webpack's new JavaScript file. This <script> tag can appear in either the HTML file's <head> or <body>. 
 	inject: 'body'
 });
