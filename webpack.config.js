c980709bc16afbdfec4706d6d0694937564609a7
Brad Harris <bradharris@Brad-Harris-MacBook-Air.local>
2017-07-26 17:05:43 -0700

    confired HTMLWebpackPluginConfig in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,14 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+	// in tutorial (no views folder):
+	// template: __dirname + '/app/index.html'
+	template: __dirname + '/app/views/index.html',
+	filename: 'index.html'
+	// When HTMLWebpackPlugin creates a new HTML file, that new HTML file will contain a <script> tag linking to webpack's new JavaScript file. This <script> tag can appear in either the HTML file's <head> or <body>. 
+	inject: 'body'
+});
+
 module.exports = {
 	entry: __dirname + '/app/index.js',
 	module: {
@@ -12,5 +23,6 @@ module.exports = {
 	output: {
 		filename: 'transformed.js',
 		path: __dirname + '/build'
-	}
+	},
+	plugins: [HTMLWebpackPluginConfig]
 };
